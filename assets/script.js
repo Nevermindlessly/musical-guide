662b49f4cd7d3b48fd8ebd61c543654657368698
jorsh <ssb.nevermind@gmail.com>
2023-03-16 19:18:18 -0400

    Got the password randomizer working woopwoop

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
+var lowers = "abcdefghijklmnopqrstuvwxyz"
+var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numerics = "0123456789"
+var special = "!@#$%^&*()"
+var passChars = "";
+var output = "";
+
 //Stand-in function to run the script upon opening the page
 var bing = function() {
    
-  //Prompts the user for desired password length
+  //Prompts the user for desired password length, provides validation if a usable response is given and an error message if not
   var passLength = window.prompt("Enter a desired password length between 8-128 characters");
-
-  //Validates the user's selection if valid, provides error message if not
   if (!passLength) {
       return;
   } else if (
@@ -19,14 +24,49 @@ var bing = function() {
       window.alert("Password length of " + passLength + " selected")
   }
 
-  var passLower = window.prompt("Would you like to include lowercase letters?")
+  //Asks the user whether they'd like to include lowercases
+  var passLower = window.confirm("Would you like to include lowercase letters?")
+  if (!passLower) {
+    window.alert("Lowercase letters will not be included");
+  } else {
+    window.alert("Lowercase letters will be included")
+    passChars += lowers
+  }
 
-  var passUpper = window.prompt("Would you like to include uppercase letters?")
+  //Asks the user whether they'd like to include uppercasess
+  var passUpper = window.confirm("Would you like to include uppercase letters?")
+  if (!passUpper) {
+    window.alert("Uppercase letters will not be included");
+  } else {
+    window.alert("Uppercase letters will be included")
+    passChars += uppers
+  }
 
-  var passNum = window.prompt("Would you like to include numerics?")
+  //Asks the user whether they'd like to include numericss
+  var passNum = window.confirm("Would you like to include numerics?")
+  if (!passNum) {
+    window.alert("Numerics will not be included");
+  } else {
+    window.alert("Numerics will be included")
+    passChars += numerics
+  }
+  
+  //Asks the user whether they'd like to include special characters
+  var passSpec = window.confirm("Would you like to include special characters?")
+  if (!passSpec) {
+    window.alert("Special characters will not be included");
+  } else {
+    window.alert("Special characters will be included")
+    passChars += special
+  }
 
-  var passSpec = window.prompt("Would you like to include special characters?")
-
+  //Generates random combination from selected character types
+  for (var i = 0; i < passLength; i++) {
+    output += passChars.charAt(Math.floor(Math.random() * passChars.length));
+  }
+  
+  //Posts the result
+  window.alert(output);
 }
 
 // Assignment Code
@@ -34,7 +74,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(
+
+  );
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
